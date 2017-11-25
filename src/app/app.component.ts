34c392af0cfc098c616847576c0ec7f5182a2364
unknown <ian.albuquerque.silva@gmail.com>
2017-11-25 20:22:39 -0200

    websocket helloworld

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,32 @@
 import { Component } from '@angular/core';
+// import * as WebSocket from 'ws';
+// import { setInterval } from 'timers';
+
+const ws = new WebSocket('ws://localhost:8999');
+
+let x = 0;
+
+ws.onopen = function (event) {
+  ws.send("Im alive!"); 
+};
+
+setInterval(() => {
+  ws.send(`Ping number ${x}`);
+  x += 1;
+}, 1000);
+
+ws.onmessage = function (event) {
+  console.log(event.data);
+}
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'app';
+  public title = 'app';
+
+  constructor() {
+  }
 }
